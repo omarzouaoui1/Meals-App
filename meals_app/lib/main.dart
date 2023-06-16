8ceb2f28618fa7de7c8ad4932f0caa3d57b4ac6e
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-16 14:01:52 +0100

    categories widget and rendring a GridView

---
+++ meals_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/categories.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: Color.fromARGB(255, 76, 173, 63),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
