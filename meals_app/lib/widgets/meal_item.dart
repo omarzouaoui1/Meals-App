bf4d42f248bc9cd1dab5d5e4c5001133b15e98e6
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-20 10:07:03 +0100

    Rendring meal + duration, complexity & affordability

--- meals_app/lib/widgets/meal_item.dart
+++ meals_app/lib/widgets/meal_item.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  //to upperCase the first letter of the complexity
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  //to upperCase the first letter of the affordability
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +65,24 @@ class MealItem extends StatelessWidget {
                       height: 12,
                     ),
                     Row(
-                      children: [],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule_rounded,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(
+                          width: 13,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.attach_money_outlined,
+                          label: affordabilityText,
+                        ),
+                      ],
                     ),
                   ],
                 ),
